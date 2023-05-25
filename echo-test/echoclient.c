860e6ded5fab4bfc57376582b8c8ac70d1510ef3
Hyunoh Jo <hyunoh.jo@gmail.com>
2023-05-25 01:27:54 +0000

    Feat: Add echo server

---
+++ echo-test/echoclient.c
@@ -0,0 +1,33 @@
+/*
+ * echoclient.c - An echo client
+ */
+/* $begin echoclientmain */
+#include "csapp.h"
+
+int main(int argc, char **argv) 
+{
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3) {
+	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+	exit(0);
+    }
+    host = argv[1];
+    port = argv[2];
+
+    // Open_clientfd 함수를 호출하여 클라이언트 소켓을 생성하고, 서버와 연결합니다.
+    // Rio_readinitb 함수를 호출하여 rio 구조체를 초기화합니다.
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+	Rio_writen(clientfd, buf, strlen(buf));
+	Rio_readlineb(&rio, buf, MAXLINE);
+	Fputs(buf, stdout);
+    }
+    Close(clientfd); //line:netp:echoclient:close
+    exit(0);
+}
+/* $end echoclientmain */
